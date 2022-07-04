b085cdbc3bf036b9bf2a9404caeacd3348159067
forrest-herman <forrestherman13@gmail.com>
2022-07-04 02:44:11 -0400

    auto create journal page template based on weekday

--- main.py
+++ main.py
@@ -25,11 +25,56 @@ def readDatabase(database_id, headers):
 
     data = result.json()
 
-    with open('./db.json', 'w', encoding='utf8') as f:
+    with open('./json/db.json', 'w', encoding='utf8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
-def create_page(database_id, headers):
+def read_database_pages(database_id, headers):
+    url = f'https://api.notion.com/v1/databases/{database_id}/query'
+
+    # get the correct template based on weekday or weekend
+    if datetime.now().weekday() < 5:
+        pageName = "Daily Summary Preteckt Template"
+    else:
+        pageName = "Daily Summary Template"
+
+    payload = {
+        "page_size": 10,
+        "filter": {
+            "property": "Name",
+            "rich_text": {
+                "equals": pageName
+            }
+        }
+    }
+
+    result = requests.post(url, headers=headers, json=payload)
+    print(result.status_code)
+
+    data = result.json()
+
+    # read_block_children(data['results'][0]['id'], headers)
+
+    with open('./json/db_query.json', 'w', encoding='utf8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    return data['results'][0]['id']
+
+
+def read_block_children(block_id, headers):
+    url = f'https://api.notion.com/v1/blocks/{block_id}/children'
+    res = requests.get(url, headers=headers)
+    print(res.status_code)
+
+    data = res.json()
+
+    with open('./json/page_data.json', 'w', encoding='utf8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    return data["results"]
+
+
+def create_page(database_id, headers, blocks_data):
     url = 'https://api.notion.com/v1/pages'
 
     newPageData = {
@@ -54,43 +99,15 @@ def create_page(database_id, headers):
                 }
             },
         },
-        "children": [
-            {
-                "object": "block",
-                "type": "template",
-                "template": {
-                    "rich_text": [
-                        {
-                            "type": "text",
-                            "text": {
-                                "content": "Create callout template"
-                            }
-                        }
-                    ],
-                    "children": [
-                        {
-                            "callout": {
-                                "rich_text": [
-                                    {
-                                        "type": "text",
-                                        "text": {
-                                            "content": "Placeholder callout text"
-                                        }
-                                    }
-                                ]
-                            }
-                        }
-                    ]
-                }
-            },
-        ]
+        "children": blocks_data
     }
 
-    res = requests.post(url, json=newPageData, headers=headers)
-    print(res.text)
+    res = requests.post(url, headers=headers, json=newPageData)
     print(res.status_code)
 
 
-print(datetime.now().isoformat())
-readDatabase(database_id, headers)
-create_page(database_id, headers)
+# print(datetime.now().isoformat())
+
+databasePage_id = read_database_pages(database_id, headers)
+templateBlocks = read_block_children(databasePage_id, headers)
+create_page(database_id, headers, templateBlocks)
