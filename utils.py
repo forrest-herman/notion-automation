a45b739f001cdac31e87b1e21934637bfbbcc916
forrest-herman <forrestherman13@gmail.com>
2022-07-20 14:11:45 -0400

    add new util method

--- utils.py
+++ utils.py
@@ -5,3 +5,8 @@ import json
 def save_json_to_file(data, file_name='data.json'):
     with open(file_name, 'w', encoding='utf8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+def write_to_file(data, file_name='data.txt'):
+    with open(file_name, 'w', encoding='utf8') as f:
+        f.write(data)
