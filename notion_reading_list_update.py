c63acd6c0b3016e6f8cbe0ad87583407bd8d3456
forrest-herman <forrestherman13@gmail.com>
2022-08-01 01:29:13 -0400

    update reading list for reads database

--- notion_reading_list_update.py
+++ notion_reading_list_update.py
@@ -1,7 +1,10 @@
 from notion_api_methods import *
 import notion_utils
 
-reading_database_id = '252dcf91b52847888bf3d49357af4a7b'
+from utils import save_json_to_file
+
+books_database_id = '252dcf91b52847888bf3d49357af4a7b'
+reads_database_id = '68d90801b2cd40e0a6a7a59616d073da'
 
 
 def update_reading_list(read_list, currently_reading_list):
@@ -13,29 +16,61 @@ def update_reading_list(read_list, currently_reading_list):
     # check Notion for currently reading
     query_payload = {
         "filter": {
-            "property": "Date started",
-            "date": {
-                "after": "2020-01-10"
+            "property": "Book Title",
+            "rich_text": {
+                "is_not_empty": True
             }
-        }
+        },
+        # "sorts": [
+        #     {
+        #         "property": "Date started",
+        #         "direction": "descending"
+        #     }
+        # ]
     }
-    notion_reading_list = query_database_pages(reading_database_id, query_payload)
+    notion_books_list = query_database_pages(books_database_id, query_payload)
+    # save_json_to_file(notion_books_list, 'json/notion_books_list.json')
+    # notion_reads_list = query_database_pages(reads_database_id, query_payload)
 
+    # the following needs to be changed
+    # ONCE WE HAVE MORE THAN 100 BOOKS!!!!!!!
+    # IDEA: Custom payload with book title for the query!
+    # also check author!
     notion_books = {
         notion_utils.get_page_name(page, "Book Title"): {
             "author": page['properties']['Author']['select']['name'],
             "page_id": page['id'],
             "status": page["properties"]["Status"]["status"]["name"],
             "date_finished": page["properties"]["Date finished"]["date"]["start"] if page["properties"]["Date finished"]["date"] else None,
-        } for page in notion_reading_list
+        } for page in notion_books_list
     }
 
+    # READ LIST
     # check all books in reading list
     for book in read_list:
         # check if book is in the reading list
+        # query the books database
+        notion_book = query_book_list(book)
+        if notion_book:
+            pass
+            # print(notion_book["properties"]["Book Title"]["title"][0]["text"]["content"])
+
         if(book['title'] in notion_books):
-            # print(book['title'], 'exists already')
+            # book exists in the books database already
             notion_book = notion_books[book['title']]
+
+            notion_reads_list_result = query_read_list(notion_book, book)
+
+            # check if the book has already been added to the reads list
+            if len(notion_reads_list_result) > 0:
+                print("Book exists on the reads list")
+                read_page = notion_reads_list_result[0]
+                if read_page.get("Date finished", {}) is not None:
+                    update_read_date(read_page["id"], book)
+                    print("Date finished updated")
+            else:
+                # book needs to be added to the reads list
+                add_read_date(notion_book["page_id"], book)
 
             # check if the book has been finished
             if(notion_book['status'] != 'Read' or notion_book['date_finished'] is None):
@@ -50,20 +85,41 @@ def update_reading_list(read_list, currently_reading_list):
         else:
             # if book doesn't exist, create it
             print('create new book page', book['title'])
-            create_book_page(book)
+            notion_book = create_book_page(book)
+            print(notion_book)
+            add_read_date(notion_book["id"], book)
 
+    # CURENTLY READING LIST
     for book in currently_reading_list:
         # create_or_update_book_page(book, notion_books)
         print('Currently Reading', book['title'])
+
+        # check if book is in the reading list
+        notion_book = query_book_list(book)
+        # if notion_book is None:
+
         if(book['title'] not in notion_books):
-            create_book_page(book)
+            notion_book = create_book_page(book)
+            print('Created new book page', book['title'])
+            add_read_date(notion_book["id"], book)
+        else:
+            print(book['title'], 'already exists')
+            notion_book = notion_books.get(book['title'], {})
+
+            update_book_page(notion_book, book)
+
+            # check if the book has already been added to the reads list
+            notion_reads_list_result = query_read_list(notion_book, book)
+            if len(notion_reads_list_result) == 0:
+                # book needs to be added to the reads list
+                add_read_date(notion_book["page_id"], book)
 
     # END OF SCRIPT
 
 
 def create_book_page(book_details):
     newPageData_book = {
-        "parent": {"database_id": reading_database_id},
+        "parent": {"database_id": books_database_id},
         "icon": {
             "type": "external",
             "external": {
@@ -117,7 +173,7 @@ def create_book_page(book_details):
             # "Status": {
             #     "type": "status",
             #     "status": {
-            #         "name": "Read"  # if book_details['date_read'] else "Currently Reading",
+            #         "name": "Read" if book_details['date_read'] else "Currently Reading",
             #     }
             # },
             "Rating": {
@@ -135,7 +191,6 @@ def create_book_page(book_details):
             # },
         },
     }
-
     newPage = create_page(newPageData_book)
     return newPage
 
@@ -144,14 +199,19 @@ def update_book_page(page_to_update, book_details):
     # update page properites
     newBookPageData = {
         "properties": {
+            "Date started": {
+                "date": {
+                    "start": book_details['date_started']
+                }
+            },
             "Date finished": {
                 "date": {
                     "start": book_details['date_read']
-                }
+                } if book_details['date_read'] else None
             },
             # "Status": {
             #     "status": {
-            #         "name": "Read",
+            #         "name": "Read" if book_details['date_read'] else "Currently Reading",
             #     }
             # },
             "Rating": {
@@ -162,5 +222,108 @@ def update_book_page(page_to_update, book_details):
             },
         },
     }
-
     return update_page(page_to_update['page_id'], newBookPageData)
+
+
+def add_read_date(notion_book_page_id, book_details):
+    """Add a read date for a book"""
+    newPageData_read = {
+        "parent": {"database_id": reads_database_id},
+        "properties": {
+            "Date": {
+                "title": [
+                    {
+                        "type": "mention",
+                        "mention": {
+                            "type": "date",
+                            "date": {
+                                "start": book_details['date_started'],
+                            }
+                        }
+                    }
+                ]
+            },
+            "Book": {
+                "relation": [
+                    {
+                        "id": notion_book_page_id
+                    }
+                ]
+            },
+            "Date started": {
+                "date": {
+                    "start": book_details['date_started'],
+                }
+            },
+            "Date finished": {
+                "date": {
+                    "start": book_details['date_read']
+                } if book_details['date_read'] else None
+            }
+        },
+    }
+
+    newPage = create_page(newPageData_read)
+    return newPage
+
+
+def update_read_date(page_to_update_id, book_details):
+    # update page details
+    updatePageData = {
+        "properties": {
+            "Date finished": {
+                "date": {
+                    "start": book_details['date_read']
+                }
+            },
+        },
+    }
+    return update_page(page_to_update_id, updatePageData)
+
+
+def query_read_list(notion_book, book_details):
+    read_list_query_payload = {
+        "filter": {
+            "and": [
+                {
+                    "property": "Book",
+                    "relation": {
+                        "contains": notion_book["page_id"]
+                    }
+                },
+                {
+                    "property": "Date started",
+                    "date": {
+                        "equals": book_details["date_started"]
+                    }
+                }
+            ]
+        },
+    }
+    result = query_database_pages(reads_database_id, read_list_query_payload)
+    return result
+
+
+def query_book_list(book_details):
+    book_query_payload = {
+        "filter": {
+            "and": [
+                {
+                    "property": "Book Title",
+                    "rich_text": {
+                        "equals": book_details['title']
+                    }
+                },
+                {
+                    "property": "Author",
+                    "select": {
+                        "equals": book_details["author_name"]
+                    }
+                }
+            ]
+        }
+    }
+    result = query_database_pages(books_database_id, book_query_payload)
+    if len(result) > 0:
+        return result[0]
+    return None
