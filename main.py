942ffb7f220dbcc70bf48269f41a117d1974c49b
Forrest Herman <forrestherman13@gmail.com>
2023-01-12 14:16:39 -0500

    update user data folder WIP

--- main.py
+++ main.py
@@ -11,19 +11,20 @@ from utils import save_json_to_file, read_json_from_file
 books_read, currently_reading = goodreads.get_read_and_reading()
 if len(currently_reading) == 0:
     try:
-        prev_book_details = read_json_from_file('json/current_book.json')
+        # TODO: make this cloud enabled
+        prev_book_details = read_json_from_file('user_data/current_book.json')
     except JSONDecodeError:
         prev_book_details = None
     if prev_book_details is not None:
         prev_book_details['progress'] = 100
-        save_json_to_file(prev_book_details, 'json/current_book.json')
+        save_json_to_file(prev_book_details, 'user_data/current_book.json')
 elif len(currently_reading) == 1:
     # if there is a book currently being read
     progress = goodreads.get_progress_from_home_page()
     currently_reading[0]['progress'] = progress
 
     # store the current book in a json file with the progress
-    save_json_to_file(currently_reading[0], 'json/current_book.json')
+    save_json_to_file(currently_reading[0], 'user_data/current_book.json')
 else:
     print("Too many books currently being read")
 
