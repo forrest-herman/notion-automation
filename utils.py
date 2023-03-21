7ad6c5f04a9d296fd663198dc8bf76f5ae6f53e8
forrest-herman <forrestherman13@gmail.com>
2023-03-20 20:35:58 -0400

    add create_path_to_file util

--- utils.py
+++ utils.py
@@ -1,10 +1,17 @@
 # python utilities
 import json
+import os
+import re
 
 
-def save_json_to_file(data, file_name='data.json'):
-    with open(file_name, 'w', encoding='utf8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+def save_json_to_file(data, file='data.json', current_working_dir=os.getcwd()):
+    try:
+        with open(file, 'w', encoding='utf8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+    except FileNotFoundError:
+        create_path_to_file(file, current_working_dir)
+        # target directory should now exist, try again
+        save_json_to_file(data, file)
 
 
 def read_json_from_file(file_name='data.json'):
@@ -16,6 +23,19 @@ def read_json_from_file(file_name='data.json'):
 def write_to_file(data, file_name='data.txt'):
     with open(file_name, 'w', encoding='utf8') as f:
         f.write(data)
+
+
+def create_path_to_file(file_path, working_dir=os.getcwd()):
+    # create the path if it doesn't exist
+    match = re.search(r'(.*)\/(.*\.[A-z]+$)', file_path)
+    folder_path = match.group(1)
+
+    folder_paths = re.split(r'\/|\\', folder_path)
+
+    # recursively create the path
+    for new_folder in folder_paths:
+        working_dir = os.path.join(working_dir, new_folder)
+        os.mkdir(working_dir)
 
 
 def get_12hr_time_format_for_os(os_name):
