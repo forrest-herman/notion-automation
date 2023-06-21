8a97a1a612278cfc5beeda8126f4d01933972287
forrest-herman <forrestherman13@gmail.com>
2023-06-21 12:10:22 -0400

    notion insurance tracker improvements

--- notion_insurance_tracker.py
+++ notion_insurance_tracker.py
@@ -5,36 +5,60 @@ database_id_insurance = '7f213122bd5d41db995bb770c0d130c1'
 database_id_pivot = '8242f0300dda4be398b210b1e92285ab'
 
 
-def query_insurance_database(date) -> bool:
+def query_insurance_database(date, type) -> bool:
     payload = {
         "page_size": 10,
         "filter": {
-            "property": "Date",
-            "date": {
-                "equals": date.isoformat()
-            }
+            "and": [
+                {
+                    "property": "Date",
+                    "date": {
+                        "equals": date.isoformat()
+                    }
+                },
+                {
+                    "property": "Type",
+                    "select": {
+                        "equals": type
+                    },
+                }
+            ]
         }
     }
 
     # check if page already exists
-    today_journal = query_database_pages(database_id_insurance, payload)
+    existing_appointment = query_database_pages(database_id_insurance, payload)
 
-    if not today_journal:
+    if not existing_appointment:
         return False
     return True
 
 
-def get_latest_pivot_table_year(date) -> str:
+def get_latest_pivot_table_year(date, type) -> str:
+    # check date is before september
+    if date.month < 9:
+        # get previous year
+        date = date.replace(year=date.year - 1)
     date = date.isoformat()
     year = date[:4]
 
     # TODO: fix this query for the date is between start and end
     payload = {
         "filter": {
-            "property": "Timeframe",
-            "date": {
-                "past_year": {}
-            }
+            "and": [
+                {
+                    "property": "Timeframe",
+                    "date": {
+                        "past_year": {}
+                    }
+                },
+                {
+                    "property": "Type",
+                    "select": {
+                        "equals": type
+                    },
+                }
+            ]
             # "and": [
             #     {
             #         "property": "Timeframe",
@@ -57,11 +81,17 @@ def get_latest_pivot_table_year(date) -> str:
         newPageData = {
             "parent": {"database_id": database_id_pivot},
             "properties": {
+                "Type": {
+                    "type": "select",
+                    "select": {
+                        "name": type
+                    }
+                },
                 "Name": {
                     "title": [
                         {
                             "text": {
-                                "content": f'{year}-{int(year) + 1}'
+                                "content": f'{type} {year}-{int(year) + 1}'
                             }
                         }
                     ]
@@ -69,7 +99,7 @@ def get_latest_pivot_table_year(date) -> str:
                 "Timeframe": {
                     "date": {
                         "start": datetime.date(int(year), 9, 1).isoformat(),
-                        "end": datetime.date(int(year) + 1, 8, 31).isoformat() # double check
+                        "end": datetime.date(int(year) + 1, 8, 31).isoformat()
                     }
                 }
             }
@@ -81,16 +111,16 @@ def get_latest_pivot_table_year(date) -> str:
     return pivot_page_id
 
 
-def add_new_appointment(type, cost, date=None):
+def add_new_appointment(type, cost, date=None, emoji=''):
     """Add a new appointment to the database"""
 
     if date is None:
         date = datetime.date.today()
 
     # check if page already exists
-    if query_insurance_database(date):
+    if query_insurance_database(date, type):
         print(f"Appointment already exists for {date.isoformat()}")
-        # return
+        return
 
     new_appointment_data = {
         "parent": {"database_id": database_id_insurance},
@@ -125,7 +155,7 @@ def add_new_appointment(type, cost, date=None):
             "Linked to Pivot Table": {
                 "relation": [
                     {
-                        "id": get_latest_pivot_table_year(date)
+                        "id": get_latest_pivot_table_year(date, type)
                     }
                 ]
             }
@@ -133,7 +163,7 @@ def add_new_appointment(type, cost, date=None):
         "icon": {
             "type": "emoji",
             "emoji": "☯"
-        }
+        } if emoji else None
     }
 
     create_page(new_appointment_data)
